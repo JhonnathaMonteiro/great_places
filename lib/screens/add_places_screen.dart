521c47753ab8d323c0216fc6f8b90b60c9ae3611
JhonnathaMonteiro <jhonnatha.am@gmail.com>
2020-06-10 09:33:41 -0300

    saving file (pic) into device

--- lib/screens/add_places_screen.dart
+++ lib/screens/add_places_screen.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:great_places/widgets/image_input.dart';
+import 'package:provider/provider.dart';
+
+import '../widgets/image_input.dart';
+import '../providers/great_places.dart';
 
 class AddPlacesScreen extends StatefulWidget {
   static const routeName = '/add-place';
@@ -9,6 +14,23 @@ class AddPlacesScreen extends StatefulWidget {
 
 class _AddPlacesScreenState extends State<AddPlacesScreen> {
   final _titleController = TextEditingController();
+  File _pickedImage;
+
+  void _selectImage(File pickedImage) {
+    _pickedImage = pickedImage;
+  }
+
+  void _savePlace() {
+    if (_titleController.text.isEmpty || _pickedImage == null) {
+      return;
+    }
+    Provider.of<GreatePlaces>(context, listen: false).addPlaces(
+      _titleController.text,
+      _pickedImage,
+    );
+    Navigator.of(context).pop();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,7 +53,9 @@ class _AddPlacesScreenState extends State<AddPlacesScreen> {
                     SizedBox(
                       height: 10,
                     ),
-                    ImageInput(),
+                    ImageInput(
+                      onSelectImage: _selectImage,
+                    ),
                   ],
                 ),
               ),
@@ -40,7 +64,7 @@ class _AddPlacesScreenState extends State<AddPlacesScreen> {
           RaisedButton.icon(
             icon: Icon(Icons.add),
             label: Text('Add Place'),
-            onPressed: () {},
+            onPressed: _savePlace,
             elevation: 0,
             materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
             color: Theme.of(context).accentColor,
--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 import 'package:great_places/screens/add_places_screen.dart';
+import 'package:great_places/providers/great_places.dart';
 
 class PlacesListScreen extends StatelessWidget {
   @override
@@ -16,7 +19,25 @@ class PlacesListScreen extends StatelessWidget {
           )
         ],
       ),
-      body: Center(child: CircularProgressIndicator()),
+      body: Consumer<GreatePlaces>(
+        child: Center(child: Text('Got not places yet, start adding some')),
+        builder: (ctx, greatPlaces, ch) => greatPlaces.items.length <= 0
+            ? ch
+            : ListView.builder(
+                itemCount: greatPlaces.items.length,
+                itemBuilder: (BuildContext ctx, int i) => ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: FileImage(
+                      greatPlaces.items[i].image,
+                    ),
+                  ),
+                  title: Text(greatPlaces.items[i].title),
+                  onTap: () {
+                    // go to detail page
+                  },
+                ),
+              ),
+      ),
     );
   }
 }
